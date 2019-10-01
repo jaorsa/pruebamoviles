c5f4de630d6accbacff155e6871f3038957dd7e4
Jacqueline Ortiz Sánchez <A01329978@itesm.mx>
2019-10-01 14:36:36 -0500

    Initial Commit

---
+++ prueba1/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  prueba1
+//
+//  Created by Alumno on 10/1/19.
+//  Copyright © 2019 Jacqueline Ortiz Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ prueba1/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  prueba1
+//
+//  Created by Alumno on 10/1/19.
+//  Copyright © 2019 Jacqueline Ortiz Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
